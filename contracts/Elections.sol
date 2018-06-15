01b45a48f9c44ba9a0c36f3c5b2fa87d8d303043
Aniket Sachan <aniket@localhost.localdomain>
2018-06-15 08:26:57 +0530

    Second commit

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -1,11 +1,27 @@
 pragma solidity ^0.4.17;
 
 contract Election {
-    //Store candidate
-    //Read Candidate
-    string public candidate;
-    //Constructor
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Fetch Candidate
+    mapping (uint => Candidate) public candidates;
+
+    // Store candidates count
+    uint public candidatesCount;
+
+
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
